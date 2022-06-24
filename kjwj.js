c6619d19f5d611bd0a85446eebea9673e68fd490
临渊 <74068830+LinYuanovo@users.noreply.github.com>
2022-06-24 13:24:18 +0800

    科技玩家

--- kjwj.js
+++ kjwj.js
@@ -11,7 +11,7 @@
  const $ = new Env('科技玩家');
  const notify = $.isNode() ? require('./sendNotify') : '';
  const {log} = console;
- const Notify = 0; //0为关闭通知，1为打开通知,默认为1
+ const Notify = 1; //0为关闭通知，1为打开通知,默认为1
  const debug = 0; //0为关闭调试，1为打开调试,默认为0
  //////////////////////
  let kjwj = process.env.kjwj;
