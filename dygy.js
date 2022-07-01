db6c34741fab2fd9cb429c91a4b86e52bc6111c2
临渊 <74068830+LinYuanovo@users.noreply.github.com>
2022-07-01 16:40:38 +0800

    抖音果园

--- dygy.js
+++ dygy.js
@@ -1,4 +1,4 @@
-/*
+/**
  作者：临渊
  日期：6-21
  抖音小程序：抖抖果园
@@ -24,16 +24,19 @@
 const $ = new Env('抖抖果园');
 const notify = $.isNode() ? require('./sendNotify') : '';
 const {log} = console;
-const Notify = 1; //0为关闭通知，1为打开通知,默认为1
+const Notify = 0; //0为关闭通知，1为打开通知,默认为1
 const debug = 0; //0为关闭调试，1为打开调试,默认为0
 const uaNum = 1; //随机UA，从0-20随便选一个填上去
 //////////////////////
+let scriptVersion = "1.1.0";
+let scriptVersionLatest = '';
 let ddgyCk = ($.isNode() ? process.env.ddgyCk : $.getdata("ddgyCk")) || "";
 let UA = ($.isNode() ? process.env.UA : $.getdata("UA")) || "";
 let ck;
 let UAArr = [];
 let ddgyCkArr = [];
 let msg = '';
+let loginBack = 0;
 let boxTimes = 0;
 let boxState = 0;
 let challengeTimes = 0;
@@ -122,84 +125,87 @@ let ua = User_Agents[uaNum];
                 await getInfo();
                 await $.wait(2 * 1000);
 
-                if (giftBack) {
-                    log('开始领取新人礼物');
-                    await getGift();
-                    await $.wait(2 * 1000);
-                }
-
-                log('开始签到');
-                await doSignin();
-                await $.wait(2 * 1000);
-
-                log('开始收取水瓶奖励');
-                await getBottle();
-                await $.wait(2 * 1000);
-
-                log('开始领取每日水滴');
-                await getTask1();
-                await $.wait(2 * 1000);
-
-                log('开始戳鸭子');
-                do {
-                    await touchDuck();
-                    await $.wait(2 * 1000);
-                } while (touchDuckBack);
-
-                if (hour >=7 && hour <9) {
-                    log('开始领取早餐礼包');
-                    await getTask2();
-                    await $.wait(2 * 1000);
-                } else if (hour >=12 && hour<14) {
-                    log('开始领取午餐礼包');
-                    await getTask2();
-                    await $.wait(2 * 1000);
-                } else if (hour >=18 && hour <21) {
-                    log('开始领取晚餐礼包');
-                    await getTask2();
-                    await $.wait(2 * 1000);
-                }
-
-                log('开始选择挑战');
-                await chooseChallenge();
-                await $.wait(2 * 1000);
-
-                if (nutrientBack) {
-                    log('开始肥料签到');
-                    await nutrientSignin();
-                    await $.wait(2 * 1000);
-
-                    log('开始获取肥料列表');
-                    await getNutrientList();
-                    await $.wait(2 * 1000);
-
-                    if (liteFertilizerType == 1) {
-                        log('开始使用小袋化肥');
-                        await useLiteNutrient();
+                if (loginBack) {
+                    if (giftBack) {
+                        log('开始领取新人礼物');
+                        await getGift();
                         await $.wait(2 * 1000);
                     }
-                }
 
-                log('开始浇水');
-                while (waterBack == 0) {
-                    await giveWater();
+                    log('开始签到');
+                    await doSignin();
                     await $.wait(2 * 1000);
-                    if (boxBack == 1 && boxTimes == 0 && boxState !=7) {
-                        log('开始开宝箱');
-                        await openBox();
+
+                    log('开始收取水瓶奖励');
+                    await getBottle();
+                    await $.wait(2 * 1000);
+
+                    log('开始领取每日水滴');
+                    await getTask1();
+                    await $.wait(2 * 1000);
+
+                    log('开始戳鸭子');
+                    do {
+                        await touchDuck();
+                        await $.wait(2 * 1000);
+                    } while (touchDuckBack);
+
+                    if (hour >=7 && hour <9) {
+                        log('开始领取早餐礼包');
+                        await getTask2();
+                        await $.wait(2 * 1000);
+                    } else if (hour >=12 && hour<14) {
+                        log('开始领取午餐礼包');
+                        await getTask2();
+                        await $.wait(2 * 1000);
+                    } else if (hour >=18 && hour <21) {
+                        log('开始领取晚餐礼包');
+                        await getTask2();
                         await $.wait(2 * 1000);
                     }
-                }
 
-                if (challengeTimes == 0 && challengeState !=5) {
-                    log('开始领取挑战');
-                    await getChallengeReward();
+                    log('开始选择挑战');
+                    await chooseChallenge();
+                    await $.wait(2 * 1000);
+
+                    if (nutrientBack) {
+                        log('开始肥料签到');
+                        await nutrientSignin();
+                        await $.wait(2 * 1000);
+
+                        log('开始获取肥料列表');
+                        await getNutrientList();
+                        await $.wait(2 * 1000);
+
+                        if (liteFertilizerType == 1) {
+                            log('开始使用小袋化肥');
+                            await useLiteNutrient();
+                            await $.wait(2 * 1000);
+                        }
+                    }
+
+                    log('开始浇水');
+                    while (waterBack == 0) {
+                        await giveWater();
+                        await $.wait(2 * 1000);
+                        if (boxBack == 1 && boxTimes == 0 && boxState !=7) {
+                            log('开始开宝箱');
+                            await openBox();
+                            await $.wait(2 * 1000);
+                        }
+                    }
+
+                    if (challengeTimes == 0 && challengeState !=5) {
+                        log('开始领取挑战');
+                        await getChallengeReward();
+                        await $.wait(2 * 1000);
+                    }
+
+                    log('开始获取信息');
+                    await getHomeInfo();
                     await $.wait(2 * 1000);
                 }
-
-                log('开始获取信息');
-                await getHomeInfo();
-                await $.wait(2 * 1000);
+                loginBack = 0; //置0
 
             }
             await SendMsg(msg);
@@ -851,6 +857,7 @@ function getInfo(timeout = 3 * 1000) {
 
                 let result = data == "undefined" ? await getInfo() : JSON.parse(data);
                 if (result.status_code == 0) {
+                    loginBack = 1;
                     if (result.data.show_info.show_green_gift == true) {
                         giftBack = 1;
                     }
@@ -862,7 +869,7 @@ function getInfo(timeout = 3 * 1000) {
                     }
                 } else {
 
-                    log(`获取主页信息失败，原因是:${result.message}`)
+                    log(`获取主页信息失败，原因是:${result.message}，退出脚本`)
 
                 }
 
@@ -1063,4 +1070,23 @@ function modify() {
     })
 }
 
+/**
+ * 获取远程版本
+ */
+function getVersion(timeout = 3 * 1000) {
+    return new Promise((resolve) => {
+        let url = {
+            url: `https://raw.gh.fakev.cn/LinYuanovo/scripts/main/dygy.js`,
+        }
+        $.get(url, async (err, resp, data) => {
+            try {
+                scriptVersionLatest = data.match(/scriptVersion = "([\d\.]+)"/)[1]
+            } catch (e) {
+                $.logErr(e, resp);
+            } finally {
+                resolve()
+            }
+        }, timeout)
+    })
+}
 function Env(t, e) { "undefined" != typeof process && JSON.stringify(process.env).indexOf("GITHUB") > -1 && process.exit(0); class s { constructor(t) { this.env = t } send(t, e = "GET") { t = "string" == typeof t ? { url: t } : t; let s = this.get; return "POST" === e && (s = this.post), new Promise((e, i) => { s.call(this, t, (t, s, r) => { t ? i(t) : e(s) }) }) } get(t) { return this.send.call(this.env, t) } post(t) { return this.send.call(this.env, t, "POST") } } return new class { constructor(t, e) { this.name = t, this.http = new s(this), this.data = null, this.dataFile = "box.dat", this.logs = [], this.isMute = !1, this.isNeedRewrite = !1, this.logSeparator = "\n", this.startTime = (new Date).getTime(), Object.assign(this, e), this.log("", `🔔${this.name}, 开始!`) } isNode() { return "undefined" != typeof module && !!module.exports } isQuanX() { return "undefined" != typeof $task } isSurge() { return "undefined" != typeof $httpClient && "undefined" == typeof $loon } isLoon() { return "undefined" != typeof $loon } toObj(t, e = null) { try { return JSON.parse(t) } catch { return e } } toStr(t, e = null) { try { return JSON.stringify(t) } catch { return e } } getjson(t, e) { let s = e; const i = this.getdata(t); if (i) try { s = JSON.parse(this.getdata(t)) } catch { } return s } setjson(t, e) { try { return this.setdata(JSON.stringify(t), e) } catch { return !1 } } getScript(t) { return new Promise(e => { this.get({ url: t }, (t, s, i) => e(i)) }) } runScript(t, e) { return new Promise(s => { let i = this.getdata("@chavy_boxjs_userCfgs.httpapi"); i = i ? i.replace(/\n/g, "").trim() : i; let r = this.getdata("@chavy_boxjs_userCfgs.httpapi_timeout"); r = r ? 1 * r : 20, r = e && e.timeout ? e.timeout : r; const [o, h] = i.split("@"), n = { url: `http://${h}/v1/scripting/evaluate`, body: { script_text: t, mock_type: "cron", timeout: r }, headers: { "X-Key": o, Accept: "*/*" } }; this.post(n, (t, e, i) => s(i)) }).catch(t => this.logErr(t)) } loaddata() { if (!this.isNode()) return {}; { this.fs = this.fs ? this.fs : require("fs"), this.path = this.path ? this.path : require("path"); const t = this.path.resolve(this.dataFile), e = this.path.resolve(process.cwd(), this.dataFile), s = this.fs.existsSync(t), i = !s && this.fs.existsSync(e); if (!s && !i) return {}; { const i = s ? t : e; try { return JSON.parse(this.fs.readFileSync(i)) } catch (t) { return {} } } } } writedata() { if (this.isNode()) { this.fs = this.fs ? this.fs : require("fs"), this.path = this.path ? this.path : require("path"); const t = this.path.resolve(this.dataFile), e = this.path.resolve(process.cwd(), this.dataFile), s = this.fs.existsSync(t), i = !s && this.fs.existsSync(e), r = JSON.stringify(this.data); s ? this.fs.writeFileSync(t, r) : i ? this.fs.writeFileSync(e, r) : this.fs.writeFileSync(t, r) } } lodash_get(t, e, s) { const i = e.replace(/\[(\d+)\]/g, ".$1").split("."); let r = t; for (const t of i) if (r = Object(r)[t], void 0 === r) return s; return r } lodash_set(t, e, s) { return Object(t) !== t ? t : (Array.isArray(e) || (e = e.toString().match(/[^.[\]]+/g) || []), e.slice(0, -1).reduce((t, s, i) => Object(t[s]) === t[s] ? t[s] : t[s] = Math.abs(e[i + 1]) >> 0 == +e[i + 1] ? [] : {}, t)[e[e.length - 1]] = s, t) } getdata(t) { let e = this.getval(t); if (/^@/.test(t)) { const [, s, i] = /^@(.*?)\.(.*?)$/.exec(t), r = s ? this.getval(s) : ""; if (r) try { const t = JSON.parse(r); e = t ? this.lodash_get(t, i, "") : e } catch (t) { e = "" } } return e } setdata(t, e) { let s = !1; if (/^@/.test(e)) { const [, i, r] = /^@(.*?)\.(.*?)$/.exec(e), o = this.getval(i), h = i ? "null" === o ? null : o || "{}" : "{}"; try { const e = JSON.parse(h); this.lodash_set(e, r, t), s = this.setval(JSON.stringify(e), i) } catch (e) { const o = {}; this.lodash_set(o, r, t), s = this.setval(JSON.stringify(o), i) } } else s = this.setval(t, e); return s } getval(t) { return this.isSurge() || this.isLoon() ? $persistentStore.read(t) : this.isQuanX() ? $prefs.valueForKey(t) : this.isNode() ? (this.data = this.loaddata(), this.data[t]) : this.data && this.data[t] || null } setval(t, e) { return this.isSurge() || this.isLoon() ? $persistentStore.write(t, e) : this.isQuanX() ? $prefs.setValueForKey(t, e) : this.isNode() ? (this.data = this.loaddata(), this.data[e] = t, this.writedata(), !0) : this.data && this.data[e] || null } initGotEnv(t) { this.got = this.got ? this.got : require("got"), this.cktough = this.cktough ? this.cktough : require("tough-cookie"), this.ckjar = this.ckjar ? this.ckjar : new this.cktough.CookieJar, t && (t.headers = t.headers ? t.headers : {}, void 0 === t.headers.Cookie && void 0 === t.cookieJar && (t.cookieJar = this.ckjar)) } get(t, e = (() => { })) { t.headers && (delete t.headers["Content-Type"], delete t.headers["Content-Length"]), this.isSurge() || this.isLoon() ? (this.isSurge() && this.isNeedRewrite && (t.headers = t.headers || {}, Object.assign(t.headers, { "X-Surge-Skip-Scripting": !1 })), $httpClient.get(t, (t, s, i) => { !t && s && (s.body = i, s.statusCode = s.status), e(t, s, i) })) : this.isQuanX() ? (this.isNeedRewrite && (t.opts = t.opts || {}, Object.assign(t.opts, { hints: !1 })), $task.fetch(t).then(t => { const { statusCode: s, statusCode: i, headers: r, body: o } = t; e(null, { status: s, statusCode: i, headers: r, body: o }, o) }, t => e(t))) : this.isNode() && (this.initGotEnv(t), this.got(t).on("redirect", (t, e) => { try { if (t.headers["set-cookie"]) { const s = t.headers["set-cookie"].map(this.cktough.Cookie.parse).toString(); s && this.ckjar.setCookieSync(s, null), e.cookieJar = this.ckjar } } catch (t) { this.logErr(t) } }).then(t => { const { statusCode: s, statusCode: i, headers: r, body: o } = t; e(null, { status: s, statusCode: i, headers: r, body: o }, o) }, t => { const { message: s, response: i } = t; e(s, i, i && i.body) })) } post(t, e = (() => { })) { if (t.body && t.headers && !t.headers["Content-Type"] && (t.headers["Content-Type"] = "application/x-www-form-urlencoded"), t.headers && delete t.headers["Content-Length"], this.isSurge() || this.isLoon()) this.isSurge() && this.isNeedRewrite && (t.headers = t.headers || {}, Object.assign(t.headers, { "X-Surge-Skip-Scripting": !1 })), $httpClient.post(t, (t, s, i) => { !t && s && (s.body = i, s.statusCode = s.status), e(t, s, i) }); else if (this.isQuanX()) t.method = "POST", this.isNeedRewrite && (t.opts = t.opts || {}, Object.assign(t.opts, { hints: !1 })), $task.fetch(t).then(t => { const { statusCode: s, statusCode: i, headers: r, body: o } = t; e(null, { status: s, statusCode: i, headers: r, body: o }, o) }, t => e(t)); else if (this.isNode()) { this.initGotEnv(t); const { url: s, ...i } = t; this.got.post(s, i).then(t => { const { statusCode: s, statusCode: i, headers: r, body: o } = t; e(null, { status: s, statusCode: i, headers: r, body: o }, o) }, t => { const { message: s, response: i } = t; e(s, i, i && i.body) }) } } time(t, e = null) { const s = e ? new Date(e) : new Date; let i = { "M+": s.getMonth() + 1, "d+": s.getDate(), "H+": s.getHours(), "m+": s.getMinutes(), "s+": s.getSeconds(), "q+": Math.floor((s.getMonth() + 3) / 3), S: s.getMilliseconds() }; /(y+)/.test(t) && (t = t.replace(RegExp.$1, (s.getFullYear() + "").substr(4 - RegExp.$1.length))); for (let e in i) new RegExp("(" + e + ")").test(t) && (t = t.replace(RegExp.$1, 1 == RegExp.$1.length ? i[e] : ("00" + i[e]).substr(("" + i[e]).length))); return t } msg(e = t, s = "", i = "", r) { const o = t => { if (!t) return t; if ("string" == typeof t) return this.isLoon() ? t : this.isQuanX() ? { "open-url": t } : this.isSurge() ? { url: t } : void 0; if ("object" == typeof t) { if (this.isLoon()) { let e = t.openUrl || t.url || t["open-url"], s = t.mediaUrl || t["media-url"]; return { openUrl: e, mediaUrl: s } } if (this.isQuanX()) { let e = t["open-url"] || t.url || t.openUrl, s = t["media-url"] || t.mediaUrl; return { "open-url": e, "media-url": s } } if (this.isSurge()) { let e = t.url || t.openUrl || t["open-url"]; return { url: e } } } }; if (this.isMute || (this.isSurge() || this.isLoon() ? $notification.post(e, s, i, o(r)) : this.isQuanX() && $notify(e, s, i, o(r))), !this.isMuteLog) { let t = ["", "==============📣系统通知📣=============="]; t.push(e), s && t.push(s), i && t.push(i), console.log(t.join("\n")), this.logs = this.logs.concat(t) } } log(...t) { t.length > 0 && (this.logs = [...this.logs, ...t]), console.log(t.join(this.logSeparator)) } logErr(t, e) { const s = !this.isSurge() && !this.isQuanX() && !this.isLoon(); s ? this.log("", `❗️${this.name}, 错误!`, t.stack) : this.log("", `❗️${this.name}, 错误!`, t) } wait(t) { return new Promise(e => setTimeout(e, t)) } done(t = {}) { const e = (new Date).getTime(), s = (e - this.startTime) / 1e3; this.log("", `🔔${this.name}, 结束! 🕛 ${s} 秒`), this.log(), (this.isSurge() || this.isQuanX() || this.isLoon()) && $done(t) } }(t, e) }
