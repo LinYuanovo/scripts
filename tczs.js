d678024eb184ecd867dc7fd76e06110ed0f7c409
临渊 <74068830+LinYuanovo@users.noreply.github.com>
2022-07-05 13:27:30 +0800

    同程旅行种树

--- tczs.js
+++ tczs.js
@@ -4,12 +4,12 @@
  微信小程序：同程旅行 （入口：里程商城->每日签到->祈愿树）
  抓包：开着抓包软件进活动，抓 https://sgame.moxigame.cn/planttree_tc//game/local/logincheck 这条链接下请求体的 body 全部
  示例：{"info":{"appId":"xx","userId":"xx","activeId":"xx","startTime":xx,"endTime":xx,"time":"xx","openId":"xx=","nickname":"临渊","pltId":"xx","avatar":"xx","platform":"{\"money\":0,\"moneyId\":\"xx\"}","sign":"xx"},"sourceChannel":"xx"}
- 变量格式：export tczs='xxx@xxx '  最好放配置文件，用单引号括起来，如果抓到的跑不了，把\换成\\，不行的话删掉\再试试，多个账号用 @ 或者 换行 分割
+ 变量格式：export tczs='xxx@xxx '  最好放配置文件，用单引号括起来，不能有换行空格，有的话删掉。如果抓到的跑不了，把\换成\\，不行的话删掉\再试试，多个账号用 @ 或者 换行 分割
  Cron：10 9-14 * * *
 
  [task_local]
  #同程旅行种树
- 13,43 8-18 * * * https://raw.githubusercontent.com/LinYuanovo/scripts/main/tczs.js, tag=同程旅行种树, enabled=true
+ 10 9-14 * * * https://raw.githubusercontent.com/LinYuanovo/scripts/main/tczs.js, tag=同程旅行种树, enabled=true
  [rewrite_local]
  https://sgame.moxigame.cn/planttree_tc//game/local/logincheck url script-request-header https://raw.githubusercontent.com/LinYuanovo/scripts/main/tczs.js
  [MITM]
